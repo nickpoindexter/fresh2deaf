abd06ee4d0f1634f539d85e1e999c62abe84237a
Zachary Dupont <zachary.dupont@mongodb.com>
2023-06-29 10:28:25 -0400

    fixing polyfill error

---
+++ requestly/frontend/web.mql.todo/src/components/Form.tsx
@@ -0,0 +1,16 @@
+// import Modal from "@leafygreen-ui/modal";
+import { useState } from "react";
+
+export const Form = () => {
+    const [open, setOpen] = useState(false);
+
+    return (
+    <>
+        <button>test</button>
+        <Modal open={open} setOpen={setOpen}>
+            Test here.
+        </Modal>
+    </>
+    
+    )
+}
