549b49d5a56e3997e75ed91e7775d062dc1fb631
Zachary Dupont <zachary.dupont@mongodb.com>
2023-06-29 10:28:25 -0400

    fixing polyfill error

--- requestly/frontend/web.mql.todo/vite.config.ts
+++ requestly/frontend/web.mql.todo/vite.config.ts
@@ -1,10 +1,28 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 import react from "@vitejs/plugin-react";
 import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-  plugins: [react(), topLevelAwait()],
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+        babel: {
+          plugins: ["@emotion/babel-plugin"],
+        },
+    }), 
+    topLevelAwait(),
+    nodePolyfills({
+      exclude: [
+        "fs", // Excludes the polyfill for `fs` and `node:fs`.
+      ],
+      globals: {
+        Buffer: true,
+      },
+      protocolImports: true,
+    }),
+  ],
   build: {
     outDir: "build",
   },
