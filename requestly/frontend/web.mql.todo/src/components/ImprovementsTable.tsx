fb3f3855c02040d9ee378b3ba418a26b2666d27e
Jamie Anderson <jamie.anderson@mongodb.com>
2023-06-30 10:54:05 -0400

    add delete functionality and upvote button

--- requestly/frontend/web.mql.todo/src/components/ImprovementsTable.tsx
+++ requestly/frontend/web.mql.todo/src/components/ImprovementsTable.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import {Table, TableHeader, Row, Cell} from '@leafygreen-ui/table'
+import { useApp } from './RealmApp';
+import { Menu, MenuItem } from '@leafygreen-ui/menu';
+import { useImprovments } from "../hooks/useImprovements_mql";
+import Button from '@leafygreen-ui/button';
 import Icon from '@leafygreen-ui/icon';
 import IconButton from '@leafygreen-ui/icon-button';
-import { useApp } from './RealmApp';
 
 interface Improvements {
     _id?: string
@@ -18,7 +21,7 @@ interface Improvements {
     target_quarter?: string
     team?: string
     ticket?: string
-    voters: string[]
+    votes?: number
 }
 
 interface Props {
@@ -27,41 +30,57 @@ interface Props {
 
 export default function ImprovementsTable({improvements }: Props) {
   const { currentUser } = useApp();
-
   const addImprovementVote  = (improvementId: string) => {
     currentUser.functions.AddImprovementVote(improvementId)
   }
-
   const removeImprovementVote  = (improvementId: string) => {
     currentUser.functions.RemoveImprovementVote(improvementId)
   }
+  const { deleteImprovement } = useImprovments();
 
   return (
      <Table
         columns={[
-        <TableHeader label="Votes" id="votes" />,
         <TableHeader label="Name" id="name" />,
         <TableHeader label="Description" id="description" />,
         <TableHeader label="Notes" id="notes" />,
+        <TableHeader label="Actions" id="action" />,
+        <TableHeader label="Votes" id="votes" />,
         ]}
         data={improvements}
      >
       {({ datum }) => <Row>
-        <Cell>
-            {datum.voters.length}
-            { !datum.voters.includes(currentUser.id) ?
-                <IconButton aria-label="Vote Up" onClick={() => addImprovementVote(datum._id.toString())}>
-                    <Icon glyph="ArrowUp" />
-                </IconButton>
-                :
-                <IconButton aria-label="Remove Vote" onClick={() => removeImprovementVote(datum._id.toString())}>
-                    <Icon glyph="X" />
-                </IconButton>
-            }
-        </Cell>
         <Cell>{datum.name}</Cell>
         <Cell>{datum.description}</Cell>
-        <Cell>{datum.notes}</Cell>
+        <Cell>{datum.notes ?? ""}</Cell>
+        <Cell>
+          <Menu
+            trigger={
+              <Button
+                type="button"
+                size="xsmall"
+              >
+                <Icon glyph="Ellipsis" />
+              </Button>
+            }
+          >
+            <MenuItem onClick={() => deleteImprovement(datum)}>
+              Delete
+            </MenuItem>
+          </Menu>
+        </Cell>
+        <Cell>
+          {datum.voters.length}
+          {!datum.voters.includes(currentUser.id) ?
+            <IconButton aria-label="Vote Up" onClick={() => addImprovementVote(datum._id.toString())}>
+              <Icon glyph="ArrowUp" />
+            </IconButton>
+            :
+            <IconButton aria-label="Remove Vote" onClick={() => removeImprovementVote(datum._id.toString())}>
+                <Icon glyph="X" />
+            </IconButton>
+          }
+        </Cell>
       </Row>}
     </Table>
   )
