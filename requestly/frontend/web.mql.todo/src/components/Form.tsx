d22119fa5ee625ed81fc21d0ead6db5f863bbee5
Zachary Dupont <zachary.dupont@mongodb.com>
2023-06-30 17:22:10 -0400

    form validation

--- requestly/frontend/web.mql.todo/src/components/Form.tsx
+++ requestly/frontend/web.mql.todo/src/components/Form.tsx
@@ -1,13 +1,31 @@
 import Modal from "@leafygreen-ui/modal";
 import TextInput from "@leafygreen-ui/text-input";
 import styled from '@emotion/styled';
-import { Description, H3, Label } from '@leafygreen-ui/typography'
+import { Description, Label } from '@leafygreen-ui/typography'
 import { Radio, RadioGroup } from "@leafygreen-ui/radio-group"
 import Button from "@leafygreen-ui/button";
+import Banner from "@leafygreen-ui/banner";
 
-import { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
-const RadioFormInputs = ({teamRef, sizeRef}) => {
+
+export interface ImprovementReqBody {
+    name: string
+    description: string
+    team: string
+    size: string 
+}
+
+interface FormProps {
+    onSubmit: (req: ImprovementReqBody) => any
+}
+
+interface RadioFormProps {
+    teamRef: React.MutableRefObject<string>
+    sizeRef: React.MutableRefObject<string>
+}
+
+const RadioFormInputs = ({teamRef, sizeRef} : RadioFormProps) => {
     const [team, setTeam] = useState("");
     const [size, setSize] = useState("");
 
@@ -63,19 +81,37 @@ const RadioFormInputs = ({teamRef, sizeRef}) => {
 }
 
 
-export const Form = ({onSubmit}) => {
+export const Form = ({onSubmit} : FormProps) => {
     const [open, setOpen] = useState(false);
-    const summaryRef = useRef();
-    const goalRef = useRef();
-    const teamRef = useRef();
-    const sizeRef = useRef();
+    const errors = useRef<string[]>([]);
+    const [submitCount, setSubmitCount] = useState(0);
+    const summaryRef = useRef<HTMLInputElement>();
+    const goalRef = useRef<HTMLInputElement>();
+    const teamRef = useRef<string>("");
+    const sizeRef = useRef<string>("");
+
     const handleSubmit = () => {
-        onSubmit({
-            name: summaryRef.current.value,
-            description: goalRef.current.value,
+        const req = {
+            name: summaryRef.current!.value,
+            description: goalRef.current!.value,
             team: teamRef.current,
             size: sizeRef.current,
-        })
+        } as ImprovementReqBody
+        errors.current = []
+        if (req.name == undefined || req.name.length == 0){
+            errors.current.push("summary field is required");
+        }
+        if (req.description == undefined || req.description.length == 0){
+            errors.current.push("goal field is required");
+        }
+        if (req.team == undefined || req.team.length == 0){
+            errors.current.push("team field is required");
+        }
+        if (req.size == undefined || req.size.length == 0){
+            errors.current.push("size field is required");
+        }
+        if(errors.current.length > 0) return false;
+        onSubmit(req);
         return true;
     };
 
@@ -83,6 +119,9 @@ export const Form = ({onSubmit}) => {
         padding: 10px;
     `;
 
+    const errorBanners = errors.current.length ? errors.current.map((e, i) => {
+            return <Banner key={i} variant="danger">{e}</Banner>
+        }) : <></>;
     return (
     <>
         <Button
@@ -90,6 +129,7 @@ export const Form = ({onSubmit}) => {
         size="small"
         onClick={() => setOpen(curr => !curr)}>Create improvement</Button>
         <Modal open={open} setOpen={setOpen} className="form-modal">
+            { errorBanners}
             <StyledTextInput 
                 label="Summary"
                 description="Provide a brief summary of your idea"
@@ -107,6 +147,7 @@ export const Form = ({onSubmit}) => {
             <Button 
             variant="primary"
             onClick={ () => {
+                setSubmitCount(submitCount + 1);
                 if(handleSubmit()){
                     setOpen(false);
                 }
